d63c2b1b5f1d493fdc7c8d3a932f6317e76dd5d6
Xiaowen Huang <wenn009@gmail.com>
2017-10-28 21:54:55 -0400

    add second scene

---
+++ MONEY_MAKER_MILLIONAIRE/Assets/sceneMangement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneMangement : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void ChangeScene(){
+		Debug.Log("change scene");
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
+	}
+
+	public void ReturnBackAScene() {
+		Debug.Log ("return");
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex - 1);
+	}
+}
